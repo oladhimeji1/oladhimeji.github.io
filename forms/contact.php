514bd11d39752a7da26516c624b919d44da30c1d
Mohammed Oladimeji Abdulrazaq <abdulrazaq@gmail.com>
2022-10-02 00:36:48 +0100

    Form touched

--- forms/contact.php
+++ forms/contact.php
@@ -11,7 +11,7 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) 
   $subject = $_POST['subject'];
   $message = $_POST['name']."\n".$_POST['message'];
 
-  if(@mail($to, $email, $subject, $message)){
+  if(mail($to, $email, $subject, $message)){
     echo 'Thak you for contacting us. We\'ll get back to you';
   } else {
     echo 'Sorry, an error occured, this maybe due to bad network. Please try again later.';
