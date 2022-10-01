9d8e35e6c6df2e8c1aba6fa39d3823a53e22e34a
Mohammed Oladimeji Abdulrazaq <abdulrazaq@gmail.com>
2022-10-02 00:46:30 +0100

    Form touched

--- forms/contact.php
+++ forms/contact.php
@@ -7,11 +7,12 @@
   $message = $_POST['message'];
   
   $to = 'abdulrazaq2a@gmail.com';
-  $email = 'From: '.$_POST['email'];
+  $header = 'From: '.$_POST['email'];
   $subject = $_POST['subject'];
   $message = $_POST['name']."\n".$_POST['message'];
 
-  if(mail($to, $email, $subject, $message)){
+  $retval = mail($to, $subject, $message, $header);
+  if($retval){
     echo 'Thak you for contacting us. We\'ll get back to you';
   } else {
     echo 'Sorry, an error occured, this maybe due to bad network. Please try again later.';
